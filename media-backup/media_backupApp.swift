fbb03b392ffee82c0e6561c52d7b81f2ba3fdc6c
kprivitera <kyleprivitera@hotmail.com>
2025-05-05 21:12:42 +1000

    Initial Commit

---
+++ media-backup/media_backupApp.swift
@@ -0,0 +1,17 @@
+//
+//  media_backupApp.swift
+//  media-backup
+//
+//  Created by Kyle Privitera on 5/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct media_backupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ media-backupTests/media_backupTests.swift
@@ -0,0 +1,17 @@
+//
+//  media_backupTests.swift
+//  media-backupTests
+//
+//  Created by Kyle Privitera on 5/5/2025.
+//
+
+import Testing
+@testable import media_backup
+
+struct media_backupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
